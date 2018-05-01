86dc569e2b4c21575f275d0d5d9cc27236386d23
jdeboi <jdeboi@gmail.com>
2018-04-30 23:50:38 -0500

    testing origin control

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -186,6 +186,26 @@ $(document).ready(function() {
     console.log(feeds);
     for (var i = 0; i < feeds.length; i++) {
       var url = feeds[i];
+      $.ajax(url, {
+        accepts:{
+            xml:"application/rss+xml"
+        },
+        dataType:"xml",
+        success:function(data) {
+            //Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+
+            $(data).find("item").each(function () { // or "item" or whatever suits your feed
+                var el = $(this);
+                console.log("------------------------");
+                console.log("title      : " + el.find("title").text());
+                console.log("link       : " + el.find("link").text());
+                console.log("description: " + el.find("description").text());
+            });
+
+
+        }
+    });
+    /*
       feednami.load(url,function(result){
         if(result.error) {
           console.log(result.error);
@@ -208,6 +228,7 @@ $(document).ready(function() {
           google.charts.setOnLoadCallback(drawRegionsMap);
         }
       });
+      */
     }
 
   });
